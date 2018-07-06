853bfae9279c8512508df9534a4ccf776e0aee0f
Sarah Jacob <smjacob91@gmail.com>
2018-07-06 11:38:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? do |element|
+    element[0] == 'r'
+  end
+end
+
+def contain_a (array)
+  array.select do |element|
+    element.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |element|
+    element.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings(array)
+    array.reject do |element|
+      element.class != String 
+    end
+end
+
+def count_elements(array)
+  array.count do |element|
+    
+
