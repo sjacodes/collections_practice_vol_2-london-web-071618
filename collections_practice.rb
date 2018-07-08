246d76c8032b55927532455fcb73935cb157ada4
Sarah Jacob <smjacob91@gmail.com>
2018-07-08 10:40:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,6 +22,49 @@ def remove_non_strings(array)
     end
 end
 
+def count_elements
+  
+describe '#count_elements' do
+
+    # Question 5
+
+    it 'count how many times something appears in an array' do
+      expect(count_elements([{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}])).to eq([{:name => "blake", :count => 2}, {:name => "ashley", :count => 1}])
+    end
+
+  end
+
+  describe '#merge_data' do
+
+    # Question 6
+
+    it 'combines two nested data structures into one' do
+      expect(merge_data(keys, data)).to eq(merged_data)
+    end
+
+  end
+
+  describe '#find_cool' do
+
+    # Question 7
+
+    it 'find all cool hashes' do
+      expect(find_cool(cool)).to eq([{:name => "blake",:temperature => "cool"}])
+    end
+
+  end
+
+  describe '#organize_schools' do
+
+    # Question 8
+
+    it 'organizes the schools by location' do
+      expect(organize_schools(schools)).to eq(organized_schools)
+    end
+
+  end
+
+end
 
 
 
